5f5c757a05fa87e45478df99065e97dea7535fe8
melanytejad89-alt <melanytejad89@gmail.com>
2026-08-19 19:47:44 -0500

    corrige estilo en conexion.php

--- SportStore/conexion.php
+++ SportStore/conexion.php
@@ -5,10 +5,8 @@ $user = "root";
 $password = "";
 $db = "tienda_deportiva";
 
-$mysql_conn = new mysqli($host,$user,$password,$db);
+$mysql_conn = new mysqli($host, $user, $password, $db);
 
-if($mysql_conn->connect_error){
-    die("Error de conexión: ".$mysql_conn->connect_error);
+if ($mysql_conn->connect_error) {
+    die("Error de conexión: " . $mysql_conn->connect_error);
 }
-
-?>
